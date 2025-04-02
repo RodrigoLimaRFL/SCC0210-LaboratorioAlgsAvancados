16f0d9096cc3c3aa14f645c1ca965ae97de2285f
Rodrigo de Freitas Lima <guico21@usp.br>
2025-04-02 14:48:09 -0300

    9

--- 9-HarmoniaAlgoritmolandia/main.cpp
+++ 9-HarmoniaAlgoritmolandia/main.cpp
@@ -1,30 +1,72 @@
+/*
+ * Nome: Rodrigo de Freitas Lima
+ * NUSP: 12547510
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
+int potencia(int base, int exp) {
+    int resultado = 1;
+    for (int i = 0; i < exp; i++) {
+        resultado *= base;
+    }
+    return resultado;
+}
+
+bool verificarDigitos(int numero, vector<bool>& digitos) {
+    for(int i = 0; i < 5; i++)
+    {
+        int digitoN = numero % 10;
+        numero /= 10; // divide o número pela potência
+        if (digitos[digitoN]) {
+            return false; // dígito já foi utilizado
+        }
+        digitos[digitoN] = true; // marca o dígito como utilizado
+    }
+
+    return true; // todos os dígitos são únicos
+}
+
 int main()
 {
     int n; // num objetivo
+    vector<bool> digitos(10, false); // vetor que armazena os digitos de 0 a 9
+    vector<int> digitosRestantes(5);
 
-    while (true)
-    {
-        cin >> n;
+    while (cin >> n && n != 0) {
+        bool encontrou = false; // flag para verificar se o número foi encontrado
 
-        if(n == 0)
+        fill(digitos.begin(), digitos.end(), false); // reinicia o vetor de dígitos
+
+        int max = 99999 / n; // valor máximo para o denominador
+
+        for(int denominador = 1234; denominador <= max; denominador++)
         {
-            break;
+            fill(digitos.begin(), digitos.end(), false);
+            if(!verificarDigitos(denominador, digitos)) // verifica se o número já foi utilizado
+            {
+                continue; // se já foi utilizado, pula para o próximo número
+            }
+
+            int numerador = denominador * n; // calcula o numerador desejado
+            
+            if(verificarDigitos(numerador, digitos)) // verifica se o numerador desejado é válido
+            {
+                cout << numerador << " / " << denominador << " = " << n << endl; // imprime o denominador e o numerador
+                encontrou = true; // marca que encontrou uma solução
+            }
         }
 
-        int numerador = 0;
-        int denominador = 0;
-        bool digitosUsados[10];
-        for(int i = 0; i < 10; i++)
-        {
-            digitosUsados[i] = false;
+        if (!encontrou) {
+            cout << "Não há soluções para " << n << "." << endl; // se não encontrou, imprime mensagem
         }
 
+        cout << endl; // imprime uma linha em branco
     }
+
     return 0;
 }
